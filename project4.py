0a4a62a3ded9fa01fc81487b54a9826e0d01b11d
Deborah Balogun <balogundeborah28@gmail.com>
2024-07-11 16:56:57 +0100

    A python refresher

---
+++ project4.py
@@ -0,0 +1,16 @@
+# Pythagoras theorem calculator
+width = int(input('Input the width of the triangle: '))
+height = int(input('Input the height of the triangle: '))
+
+print('length =', ((width ** 2) + (height ** 2)) ** (1/2))
+
+
+# Containers
+# Tuple = ('bob',123,) **immutable
+# List = ['bob',123,'Lisa']
+# Set = {1,2,3, 'test} Every entry is unique
+# Dictionary = {'name':'Bob', 123:'test} Key-value pairs instead of single entry
+
+user_list = ['Lisa', 'Bob', 'Alex','Jane']
+# Lisa,Bob,Alex
+print(user_list[0:3])  
